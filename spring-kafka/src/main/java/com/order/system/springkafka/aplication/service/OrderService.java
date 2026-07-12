4fe8638f024043004c31ebbcb134199b00186db8
Martin9 <martinjcantillo9@gmail.com>
2026-07-11 20:58:16 -0500

    feat: adding config of the project

---
+++ spring-kafka/src/main/java/com/order/system/springkafka/aplication/service/OrderService.java
@@ -0,0 +1,4 @@
+package com.order.system.springkafka.aplication.service;
+
+public class OrderService {
+}
---
+++ spring-kafka/src/main/java/com/order/system/springkafka/domain/event/OrderCreatedEvent.java
@@ -0,0 +1,4 @@
+package com.order.system.springkafka.domain.event;
+
+public class OrderCreatedEvent {
+}
